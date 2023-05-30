600429ffe49f817769ee6c5c4d8b99cc18f4ad4d
Yujinndev <xyujinwho@gmail.com>
2023-05-30 23:30:53 +0800

    Update database

--- files/fidoapp.sql
+++ files/fidoapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2023 at 10:35 AM
+-- Generation Time: May 30, 2023 at 05:29 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -64,33 +64,7 @@ CREATE TABLE `donationtransac` (
 INSERT INTO `donationtransac` (`transacId`, `userId`, `itemId`, `dateTransac`, `quantity`, `totalAmount`, `remarks`) VALUES
 ('6470d238e3f10', 2, 1, '2023-05-24 04:01:50', 5, 3000, 'Donated'),
 ('6470d238e3f3b', 2, 2, '2023-05-26 17:01:21', 2, 998, 'Donated'),
-('6470d238e3t32', 3, 3, '2023-05-29 09:06:10', 2, 998, 'Donated'),
-('647457772d446', 1, 3, '2023-05-29 09:42:47', NULL, 998, 'Withdrawn'),
-('647457c56dc6f', 1, 2, '2023-05-29 09:44:05', NULL, 998, 'Withdrawn');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `educmat`
---
-
-CREATE TABLE `educmat` (
-  `matId` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` varchar(2500) NOT NULL,
-  `author` int(11) NOT NULL,
-  `datePosted` datetime NOT NULL,
-  `reference` varchar(255) NOT NULL,
-  `status` enum('Published','Unpublished') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `educmat`
---
-
-INSERT INTO `educmat` (`matId`, `title`, `content`, `author`, `datePosted`, `reference`, `status`) VALUES
-(1, 'How to Stay Healthy Around Pets and Other Animalsss', 'There are many health benefits of owning a pet. They can increase opportunities to exercise, get outside, and socialize. Regular walking or playing with pets can decrease blood pressure, cholesterol levels, and triglyceride levels.  Pets can help manage.', 1, '2023-05-24 13:52:25', 'https://www.cdc.gov/healthypets/keeping-pets-and-people-healthy/how.ht', 'Published'),
-(2, 'All About Dogs', 'Dogs can have many positive effects on the lives of their owners. They influence social, emotional, and cognitive development in children, promote an active lifestyle, provide companionship, and have even been able to detect oncoming epileptic seizures or', 2, '2023-05-24 03:52:25', 'https://www.cdc.gov/healthypets/pets/dogs.html', 'Unpublished');
+('6470d238e3t32', 3, 3, '2023-05-29 09:06:10', 2, 998, 'Donated');
 
 -- --------------------------------------------------------
 
@@ -104,7 +78,7 @@ CREATE TABLE `itemdonations` (
   `photo` varchar(255) DEFAULT NULL,
   `description` varchar(2500) NOT NULL,
   `price` int(11) NOT NULL,
-  `currentStocks` int(11) DEFAULT NULL,
+  `currentStocks` int(11) DEFAULT 0,
   `quarterlyStocks` int(11) NOT NULL,
   `lastWithdrawn` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -114,9 +88,34 @@ CREATE TABLE `itemdonations` (
 --
 
 INSERT INTO `itemdonations` (`itemId`, `name`, `photo`, `description`, `price`, `currentStocks`, `quarterlyStocks`, `lastWithdrawn`) VALUES
-(1, 'Vitamins', NULL, 'Pet\'s vitamins provide essential nutrients and supplements to support the overall health and well-being of your beloved pets, ensuring they receive the necessary vitamins and minerals for optimal vitality.', 600, 5, 15, '2023-05-24 03:59:01'),
-(2, 'Foods', NULL, 'High-quality dog and cat food offers a balanced and nutritious diet, promoting proper growth, strong immune systems, and healthy digestion for your furry companions.', 499, 0, 15, '2023-05-24 03:59:01'),
-(3, 'Grooming', NULL, 'Grooming products help maintain the hygiene and appearance of your pets, keeping their coats clean, shiny, and free from tangles, while also providing soothing care for their skin.', 499, 0, 15, '2023-05-24 04:00:33');
+(1, 'Vitamins', NULL, 'Pet\'s vitamins provide essential nutrients and supplements to support the overall health and well-being of your beloved pets, ensuring they receive the necessary vitamins and minerals for optimal vitality.', 600, 5, 15, NULL),
+(2, 'Foods', NULL, 'High-quality dog and cat food offers a balanced and nutritious diet, promoting proper growth, strong immune systems, and healthy digestion for your furry companions.', 499, 2, 15, NULL),
+(3, 'Grooming', NULL, 'Grooming products help maintain the hygiene and appearance of your pets, keeping their coats clean, shiny, and free from tangles, while also providing soothing care for their skin.', 499, 2, 15, NULL),
+(4, 'Water', NULL, 'Water is Life', 50, 0, 25, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `materials`
+--
+
+CREATE TABLE `materials` (
+  `matId` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` varchar(2500) NOT NULL,
+  `author` int(11) NOT NULL,
+  `datePosted` datetime NOT NULL,
+  `reference` varchar(255) NOT NULL,
+  `status` enum('Published','Unpublished') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `materials`
+--
+
+INSERT INTO `materials` (`matId`, `title`, `content`, `author`, `datePosted`, `reference`, `status`) VALUES
+(1, 'How to Stay Healthy Around Pets and Other Animalsss', 'There are many health benefits of owning a pet. They can increase opportunities to exercise, get outside, and socialize. Regular walking or playing with pets can decrease blood pressure, cholesterol levels, and triglyceride levels.  Pets can help manage.', 1, '2023-05-24 13:52:25', 'https://www.cdc.gov/healthypets/keeping-pets-and-people-healthy/how.ht', 'Published'),
+(2, 'All About Dogs', 'Dogs can have many positive effects on the lives of their owners. They influence social, emotional, and cognitive development in children, promote an active lifestyle, provide companionship, and have even been able to detect oncoming epileptic seizures or', 2, '2023-05-24 03:52:25', 'https://www.cdc.gov/healthypets/pets/dogs.html', 'Unpublished');
 
 -- --------------------------------------------------------
 
@@ -143,7 +142,8 @@ INSERT INTO `pets` (`petId`, `name`, `type`, `age`, `status`, `availability`, `p
 (1002, 'Winter Bell', 'Cat', '3 years old', 'Unvaccinated', 'Available', 'https://images.unsplash.com/photo-1624065935863-bf91d00e2c76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80'),
 (1003, 'Peanut', 'Cat', '3 years old', 'Vaccinated', 'Not Available', 'https://images.unsplash.com/photo-1609779361684-8196b3a0abf1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
 (1004, 'Blackie', 'Dog', '5 years old', 'Vaccinated', 'Not Available', 'https://plus.unsplash.com/premium_photo-1676389282268-68e11d91d8a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=686&q=80'),
-(1006, 'Chi Chi', 'Cat', '3 months old', 'Unvaccinated', 'Available', NULL);
+(1006, 'Chi Chi', 'Cat', '3 months old', 'Unvaccinated', 'Available', NULL),
+(1007, 'Akoy', 'Cat', '5 years old', 'Vaccinated', 'Available', NULL);
 
 -- --------------------------------------------------------
 
@@ -155,7 +155,7 @@ CREATE TABLE `requests` (
   `requestId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `petId` int(11) NOT NULL,
-  `dateRequested` datetime NOT NULL,
+  `dateRequested` datetime NOT NULL DEFAULT current_timestamp(),
   `requestStatus` enum('Accepted','Denied','Waiting') NOT NULL,
   `reason` varchar(2500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -195,7 +195,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`userId`, `email`, `password`, `firstname`, `lastname`, `age`, `phoneNum`, `address`, `province`, `photo`, `role`, `adoptedPets`) VALUES
 (1, 'markeugene@gmail.com', 'admin123', 'Mark Eugene', 'Laysa', 19, '+639567953354', 'Borono, Tagudin 2714', 'Ilocos Sur', 'https://mdbcdn.b-cdn.net/img/new/avatars/8.webp', 'Admin', 0),
 (2, 'altheajazel@gmail.com', 'qwerty123', 'Althea Jazel', 'Rito', 20, '+639308375686', 'Lingsat, San Fernando 2500', 'La Union', 'https://mdbcdn.b-cdn.net/img/new/avatars/5.webp', 'User', 2),
-(3, 'johndoe@gmail.com', 'john123', 'John', 'Doe', 22, '+632354654747', 'Lingsat, San Fernando', 'La Union', NULL, 'User', 0);
+(3, 'johndoe@gmail.com', 'john123', 'John', 'Doe', 22, '+632354654747', 'Lingsat, San Fernando', 'La Union', NULL, 'User', 0),
+(5, 'bryan@gmail.com', 'bryan123', 'Bryan Angel', 'Barrera', 19, '+639364325121', 'San Julian, Batac 2906', 'Ilocos Norte', NULL, 'User', NULL);
 
 --
 -- Indexes for dumped tables
@@ -216,17 +217,17 @@ ALTER TABLE `donationtransac`
   ADD KEY `fk_donationTransac_itemId` (`itemId`);
 
 --
--- Indexes for table `educmat`
---
-ALTER TABLE `educmat`
-  ADD PRIMARY KEY (`matId`),
-  ADD KEY `fk_educmat_author` (`author`);
-
---
 -- Indexes for table `itemdonations`
 --
 ALTER TABLE `itemdonations`
   ADD PRIMARY KEY (`itemId`);
+
+--
+-- Indexes for table `materials`
+--
+ALTER TABLE `materials`
+  ADD PRIMARY KEY (`matId`),
+  ADD KEY `fk_educmat_author` (`author`);
 
 --
 -- Indexes for table `pets`
@@ -260,22 +261,22 @@ ALTER TABLE `categories`
   MODIFY `categoryId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `educmat`
---
-ALTER TABLE `educmat`
-  MODIFY `matId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- AUTO_INCREMENT for table `itemdonations`
 --
 ALTER TABLE `itemdonations`
-  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `materials`
+--
+ALTER TABLE `materials`
+  MODIFY `matId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pets`
 --
 ALTER TABLE `pets`
-  MODIFY `petId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1007;
+  MODIFY `petId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1008;
 
 --
 -- AUTO_INCREMENT for table `requests`
@@ -287,7 +288,7 @@ ALTER TABLE `requests`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -301,9 +302,9 @@ ALTER TABLE `donationtransac`
   ADD CONSTRAINT `fk_donationTransac_userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `educmat`
+-- Constraints for table `materials`
 --
-ALTER TABLE `educmat`
+ALTER TABLE `materials`
   ADD CONSTRAINT `fk_educmat_author` FOREIGN KEY (`author`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
